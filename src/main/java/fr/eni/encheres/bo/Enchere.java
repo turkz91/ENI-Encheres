6083a33b985453f056cf028f3392cd278de58e9f
Teruaki01 <teruaki.kubota@gmail.com>
2022-05-23 20:00:24 +0200

    Changement type Date en LocalDateTime (date_enchere)

--- src/main/java/fr/eni/encheres/bo/Enchere.java
+++ src/main/java/fr/eni/encheres/bo/Enchere.java
@@ -1,15 +1,16 @@
 package fr.eni.encheres.bo;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Enchere {
 
 	private int no_utilisateur;
 	private int no_article;
-	private Date date_enchere;
+	private LocalDateTime date_enchere;
 	private int montant_enchere;
 	
-	public Enchere(int no_utilisateur, int no_article, Date date_enchere, int montant_enchere) {
+	public Enchere(int no_utilisateur, int no_article, LocalDateTime date_enchere, int montant_enchere) {
 		super();
 		this.no_utilisateur = no_utilisateur;
 		this.no_article = no_article;
@@ -38,11 +39,11 @@ public class Enchere {
 		this.no_article = no_article;
 	}
 
-	public Date getDate_enchere() {
+	public LocalDateTime getDate_enchere() {
 		return date_enchere;
 	}
 
-	public void setDate_enchere(Date date_enchere) {
+	public void setDate_enchere(LocalDateTime date_enchere) {
 		this.date_enchere = date_enchere;
 	}
 
